ca5c2b8986152fe455c5489004c02e109d3dbd14
feifei lee <3300595140@qq.com>
2023-03-01 16:54:13 +0800

    实现Bot相关4个API

---
+++ KOB/src/main/java/com/kob/backend/service/user/bot/AddService.java
@@ -0,0 +1,8 @@
+package com.kob.backend.service.user.bot;
+
+import java.util.Map;
+
+public interface AddService {
+    //接口默认public
+    Map<String,String> add(Map<String,String> data);
+}
--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -5,11 +5,87 @@
 <script>
 
 import ContentField from '@/components/ContentField.vue';
-
+import $ from 'jquery';
+import { useStore } from 'vuex';
 export default({
       name:"UserBotIndexView",
       components:{
           ContentField
+      },
+      setup() {
+        let store=useStore();
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/add/",
+        //     type:"post",
+        //     data:{
+        //         title:"Bot标题",
+        //         description:"描述",
+        //         content:"内容",
+        //     },
+        //     headers:{
+        //         Authorization:"Bearer "+store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     },
+        // });
+        // $.ajax({
+        //     url:"http://127.0.0.1:3000/user/bot/remove/",
+        //     type:"post",
+        //     data:{
+        //         bot_id:6,
+        //     },
+        //     headers:{
+        //         Authorization:"Bearer "+store.state.user.token,
+        //     },
+        //     success(resp){
+        //         console.log(resp);
+        //     },
+        //     error(resp){
+        //         console.log(resp);
+        //     },
+    //     // });
+    //     $.ajax({
+    //         url:"http://127.0.0.1:3000/user/bot/update/",
+    //         type:"post",
+    //         data:{
+    //             bot_id:2,
+    //             title:"new Bot标题",
+    //             description:"new 描述",
+    //             content:"new 内容",
+
+    //         },
+    //         headers:{
+    //             Authorization:"Bearer "+store.state.user.token,
+    //         },
+    //         success(resp){
+    //             console.log(resp);
+    //         },
+    //         error(resp){
+    //             console.log(resp);
+    //         },
+    //     });
+    //   }
+    
+    $.ajax({
+            url:"http://127.0.0.1:3000/user/bot/getlist/",
+            type:"get",
+            data:{
+                userId:1,
+            },
+            headers:{
+                Authorization:"Bearer "+store.state.user.token,
+            },
+            success(resp){
+                console.log(resp);
+            },
+            error(resp){
+                console.log(resp);
+            },
+        });
       }
 })
 
